245f49428b76cf14d5af862eee14a853b391c02d
Parichit <parichitran@gmail.com>
2017-09-12 15:59:05 +0530

    update

---
+++ gcdwithoutrec.py
@@ -0,0 +1,14 @@
+a,b=raw_input("Enter the values\n").split()
+a=int(a)
+b=int(b)
+n=max(a,b)
+b=min(a,b)
+if n%b==0:
+    print "The gcd is %d"%b
+else :
+    for i in range(b/2,0,-1):
+        y=n%i
+        x=b%i
+        if x==0 is y==0:
+            print "The gcd is %d"%i
+            break
